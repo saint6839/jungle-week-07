ece6462325c759655799036870e65b04b21765c2
saint6839 <saint6839@gmail.com>
2023-04-19 06:08:42 +0000

    feat : SIGPIPE 예외 처리

--- .proxy/tiny.c
+++ .proxy/tiny.c
@@ -44,11 +44,11 @@ int main(int argc, char **argv)
 
     printf("Accepted connection from (%s, %s)\n", hostname, port);
     pthread_create(&tid, NULL, thread_function, connfd_ptr);
-    pthread_detach(tid); // 스레드 분리
   }
 }
 void thread_function(void *arg)
 {
+  pthread_detach(pthread_self()); 
   int connfd = *((int *)arg);
   free(arg);
   doit(connfd);
